6073ae5771ea19c5dfff0cc59bc66235561703f3
Alberto Vena <kennyadsl@gmail.com>
2014-06-11 23:38:19 +0200

    removes deprecated engine name

--- lib/spree_sitemap_generator/engine.rb
+++ lib/spree_sitemap_generator/engine.rb
@@ -1,20 +1,13 @@
 module SpreeSitemapGenerator
   class Engine < Rails::Engine
-    engine_name 'spree_sitemap_generator'
-
     config.autoload_paths += %W(#{config.root}/lib)
-
-    # use rspec for tests
-    config.generators do |g|
-      g.test_framework :rspec
-    end
 
     def self.activate
       ActiveRecord::Relation.class_eval do
         def last_updated
           last_update = order('updated_at DESC').first
-          last_update.try(:updated_at) 
-        end 
+          last_update.try(:updated_at)
+        end
       end
 
       ActiveRecord::Base.class_eval do
